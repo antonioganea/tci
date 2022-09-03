41bb0c24fa18ac85d19aa7700a32c2355fc177b9
Antonio Ganea <antonio.ganea@yahoo.com>
2022-09-03 13:39:38 +0300

    Three car related function prototypes

--- TCI-DLL/lua-api.h
+++ TCI-DLL/lua-api.h
@@ -63,6 +63,15 @@ int l_GetPlayerPosition(lua_State* L);
 // SetPlayerPosition(myPlayer, x, y, z)
 int l_SetPlayerPosition(lua_State* L);
 
+// car = GetPlayerCar(playerID)
+int l_GetPlayerCar(lua_State* L);
+
+// fuel = GetCarFuel(car)
+int l_GetCarFuel(lua_State* L);
+
+// SetCarFuel(car, fuel)
+int l_SetCarFuel(lua_State* L);
+
 // SpawnPlayerItem(myPlayer, "SKS", 3, true) -- last parameter determines if the item is spawned in inventory or on the floor
 // might wanna consider a return type that is a user defined item type ( So you can check stuff later )
 int l_SpawnPlayerItem(lua_State* L);
