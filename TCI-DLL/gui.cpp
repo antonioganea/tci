c3874fad73053dd6cf3e65f32fbf3a8d23860860
Antonio Ganea <antonio.ganea@yahoo.com>
2022-09-15 11:01:03 +0300

    Moved imgui source files into separate folder

--- TCI-DLL/gui.cpp
+++ TCI-DLL/gui.cpp
@@ -2,9 +2,10 @@
 
 #include "gui.h"
 
-#include "imgui.h"
-#include "imgui_impl_dx9.h"
-#include "imgui_impl_win32.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
 
 
 bool gui::exit = true;
--- TCI-DLL/gui.h
+++ TCI-DLL/gui.h
@@ -42,9 +42,9 @@ namespace gui {
 
 
 
-#include "imgui.h"
-#include "imgui_impl_dx9.h"
-#include "imgui_impl_win32.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
 
 #include <stdio.h>
 
