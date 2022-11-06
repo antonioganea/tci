b45b6db0eaa37aa19c4577c6fa23fe102f647833
Antonio Ganea <antonio.ganea@yahoo.com>
2022-11-07 01:19:30 +0200

    Moved bridge layout initialization from bridge.cpp to bridge-layout.cpp

--- TCI-DLL/bridge.cpp
+++ TCI-DLL/bridge.cpp
@@ -1,42 +1,5 @@
 #include "bridge.h"
 
-BYTE* DLL_BRIDGE = NULL;
-
-/* BRIDGE LAYOUT */
-
-int* DLL_DETOURED;
-int* DLL_IS_MAGIC_CALL;
-int* DLL_COMMAND;
-int* DLL_TRIGGER_DEBUG_CALL;
-
-int* DLL_INTN_IN;
-int* DLL_INTN_OUT;
-
-int* DLL_INTS_IN; // array
-int* DLL_INTS_OUT; // array
-
-int* DLL_FLOATN_IN;
-int* DLL_FLOATN_OUT;
-
-float* DLL_FLOATS_IN; // array
-float* DLL_FLOATS_OUT; // array
-
-int* DLL_STRN_IN;
-int* DLL_STRN_OUT;
-int* DLL_STRLEN_IN; // array
-int* DLL_STRLEN_OUT; // array
-
-
-char** DLL_STRING_LOC = NULL;
-char* DLL_STRING = NULL;
-
-char** DLL_IN_STR1 = NULL;
-char** DLL_IN_STR2 = NULL;
-char** DLL_IN_STR3 = NULL;
-char** DLL_IN_STR4 = NULL;
-
-/* END OF BRIDGE LAYOUT */
-
 #include <KnownFolders.h>
 #include <shlobj.h>
 
