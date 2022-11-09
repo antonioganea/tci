ae6925d4aa21fb440a3d479fa0370dff80dd6d8f
Antonio Ganea <antonio.ganea@yahoo.com>
2022-11-09 12:33:02 +0200

    Made Auto-Injector load dll by relative path

--- Auto-Injector/Auto-Injector.cpp
+++ Auto-Injector/Auto-Injector.cpp
@@ -28,10 +28,38 @@ DWORD GetProcId(const char* procName)
 	return procId;
 }
 
+#include <string>
+#include <codecvt>
+
+std::wstring ExePath() {
+	WCHAR buffer[MAX_PATH] = { 0 };
+	GetModuleFileNameW(NULL, buffer, MAX_PATH);
+	std::wstring::size_type pos = std::wstring(buffer).find_last_of(L"\\/");
+	return std::wstring(buffer).substr(0, pos);
+}
+
 int main() {
-	const char* dllPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\DayZServer\\x64\\Debug\\TCI-DLL.dll";
+	//const char* dllPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\DayZServer\\x64\\Debug\\TCI-DLL.dll";
 	const char* procName = "DayZServer_x64.exe";
 	DWORD procId = 0;
+
+	std::wstring dllPathU = ExePath();
+	dllPathU += L"\\TCI-DLL.dll";
+
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+
+	//use converter (.to_bytes: wstr->str, .from_bytes: str->wstr)
+	std::string converted_str = converter.to_bytes(dllPathU);
+
+	//wprintf(dllPathU.c_str());
+
+	//std::cout << sizeof(dllPathU);
+
+	//std::cout << dllPath << std::endl;
+	//std::cout << converted_str.c_str() << std::endl;
+
+	//system("pause");return 0;
 
 	while (!procId)
 	{
@@ -47,7 +75,7 @@ int main() {
 
 		if (loc)
 		{
-			WriteProcessMemory(hProc, loc, dllPath, strlen(dllPath) + 1, 0);
+			WriteProcessMemory(hProc, loc, converted_str.c_str(), strlen(converted_str.c_str()) + 1, 0);
 		}
 
 		HANDLE hThread = CreateRemoteThread(hProc, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibraryA, loc, 0, 0);
